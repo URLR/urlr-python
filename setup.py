f656a034abe76e1f5d313e9dedeb02bd2378175b
Hugo Posnic <hugo.posnic@protonmail.com>
2025-07-23 15:01:33 +0000

    Deploying from urlr/api-clients-generation@e35789a2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "urlr"
-VERSION = "2.10.2"
+VERSION = "2.10.3"
 PYTHON_REQUIRES = ">= 3.9"
 REQUIRES = [
     "urllib3 >= 2.1.0, < 3.0.0",
